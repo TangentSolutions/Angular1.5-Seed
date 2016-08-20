a76924b83d394e107a3c7f35ed8098799e6ceac3
FrancoisVenter <jventerm@gmail.com>
2016-08-20 10:51:15 +0200

    Removed extra karma config. And adapted index.html

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,59 +1,45 @@
 // Karma configuration
 // Generated on Thu Aug 18 2016 22:12:59 GMT+0200 (SAST)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
+        basePath: '../src',
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../src',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [
-      'jspm',
-      'jasmine'
-    ],
-
-        files: [
-          '../node_modules/babel-core/browser-polyfill.js'
+        frameworks: [
+            'jspm',
+            'jasmine'
         ],
 
         preprocessors: {
-          'modules/**/*.spec.js': ['babel'],
-          'modules/**/!(*.spec).js': ['babel', 'coverage']
+            'modules/**/*.spec.js': ['babel'],
+            'modules/**/!(*.spec).js': ['babel', 'coverage']
         },
 
-        babelPreprocessor: { options: { stage: 1, sourceMap: 'inline' } },
+        babelPreprocessor: {options: {stage: 1, sourceMap: 'inline'}},
 
         jspm: {
-          config: 'config.js',
-          loadFiles: ['index.js', 'modules/**/*.js'],
-          serveFiles: ['modules/**/*.+(js|html|css)'],
-          stripExtension: true
-        },
-
-        proxies: {
-          '/modules/': '/base/modules/',
-          '/jspm_packages/': '/base/jspm_packages/'
+            config: 'config.js',
+            loadFiles: ['index.js', 'modules/**/*.js'],
+            serveFiles: ['modules/**/*.+(js|html|css)'],
+            stripExtension: true
         },
 
         reporters: ['dots', 'coverage'],
 
         coverageReporter: {
-          instrumenters: { isparta : require('isparta') },
-          instrumenter: { 'modules/**/*.js': 'isparta' },
-          dir: '../coverage/',
-          reporters: [
-            {type: 'html'},
-            {type: 'json'},
-            {type: 'lcov'},
-             {type: 'text-summary'}
-          ]
+            instrumenters: {isparta: require('isparta')},
+            instrumenter: {'modules/**/*.js': 'isparta'},
+            dir: '../coverage/',
+            reporters: [
+                {type: 'html'},
+                {type: 'json'},
+                {type: 'lcov'},
+                {type: 'text-summary'}
+            ]
         },
 
         browsers: ['PhantomJS'],
-        singleRun : false,
+        singleRun: true,
         browserNoActivityTimeout: 75000
-  })
+    })
 }
