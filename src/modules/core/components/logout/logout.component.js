f81cddcb402a7febf7db7b32a312b4ee9a75ff67
FrancoisVenter <jventerm@gmail.com>
2016-08-22 19:55:18 +0200

    Logout component.

---
+++ src/modules/core/components/logout/logout.component.js
@@ -0,0 +1,8 @@
+import LogoutTemplate from './logout.template.html!text';
+import LogoutController from './logout.controller';
+
+export default {
+    template: LogoutTemplate,
+    controller: LogoutController,
+    controllerAs: '$ctrl'
+};
