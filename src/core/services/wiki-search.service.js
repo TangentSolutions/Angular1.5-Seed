1e8a9604b47740e7b1078c025fad91d2a6dfa3bd
Ian Roberts <ian@tangentsolutions.co.za>
2016-09-22 16:27:05 +0200

    Reverted code climate fixme and duplication testing.

--- src/core/services/wiki-search.service.js
+++ src/core/services/wiki-search.service.js
@@ -7,21 +7,7 @@ class WikiSearchService {
         this.$http = $http;
     }
 
-    //TODO: test
     get(term) {
-        return Rx.Observable.fromPromise(() => {
-            return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
-        });
-    }
-    
-    //FIXME: sort this out
-    bet(term) {
-        return Rx.Observable.fromPromise(() => {
-            return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
-        });
-    }
-    
-    set(term) {
         return Rx.Observable.fromPromise(() => {
             return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
         });
