383531bfa63a925d4c85b2a577a638d5054a476f
FrancoisVenter <jventerm@gmail.com>
2016-09-22 16:04:54 +0200

    Added angular-mocks

--- src/core/components/app/app.component.spec.js
+++ src/core/components/app/app.component.spec.js
@@ -1,7 +1,14 @@
-import 'angular-mocks';
 import App from './app.controller';
 
 describe('some test', () => {
+    beforeEach(angular.mock.module('app'));
+
+    var $q;
+
+    beforeEach(angular.mock.inject((_$q_) => {
+        $q = _$q_;
+    }))
+
     it('should expect something', function () {
         let app = new App();
 
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       '../src/index.js',
+      '../node_modules/angular-mocks/angular-mocks.js',
       '../src/**/*.spec.js'
     ],
 
