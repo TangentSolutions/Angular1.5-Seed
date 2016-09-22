5ffe706d8fc0b888360ee62a092597dc54f12df3
Ian Roberts <ian@tangentsolutions.co.za>
2016-09-22 16:25:32 +0200

    Testing duplication and fixme on code climate

--- src/core/services/wiki-search.service.js
+++ src/core/services/wiki-search.service.js
@@ -7,7 +7,21 @@ class WikiSearchService {
         this.$http = $http;
     }
 
+    //TODO: test
     get(term) {
+        return Rx.Observable.fromPromise(() => {
+            return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
+        });
+    }
+    
+    //FIXME: sort this out
+    bet(term) {
+        return Rx.Observable.fromPromise(() => {
+            return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
+        });
+    }
+    
+    set(term) {
         return Rx.Observable.fromPromise(() => {
             return this.$http.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=10&namespace=0&format=json`);
         });
