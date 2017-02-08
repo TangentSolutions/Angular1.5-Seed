ab477ce19fd6a48374cc2a3217aa8ca25b3c66aa
Michael Frohlich <michael@tangentsolutions.co.za>
2017-02-08 10:19:20 +0200

    get function in employee list controller added

--- src/employee/employee.settings.js
+++ src/employee/employee.settings.js
@@ -1,2 +1,2 @@
 export default angular.module('employee.settings', [])
-    .constant('EMPLOYEE_SERVICE_BASE_URI', 'http://staging.tangent.tngnt.co/api/employee/');
+    .constant('EMPLOYEE_SERVICE_BASE_URI', 'http://staging.tangent.tngnt.co/api/employee/staff/');
