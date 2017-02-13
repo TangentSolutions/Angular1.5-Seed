c374ac0b4da67b6d5cad28dd1f5e4297ffc02fb9
johan1228 <johanvdv8@outlook.com>
2017-02-13 09:53:47 +0200

    Added leave folder

---
+++ src/core/components/leave/leave.component.js
@@ -0,0 +1,9 @@
+import LeaveTemplate from './leave.template.html';
+import LeaveController from './leave.controller';
+
+
+export default {
+    template: LeaveTemplate,
+    controller: LeaveController,
+    controllerAs: '$ctrl'
+};
